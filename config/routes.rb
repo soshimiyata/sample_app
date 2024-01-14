8e9897ddec4d04a98bae05734750b04dfd84fda3
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-01-14 01:36:09 +0000

    sample_app 6start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
